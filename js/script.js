6146339d94d329414ef353c02290048424b26ec2
Zosia Samosia <Zosia@Samosia.pl>
2024-04-15 08:52:22 +0200

    mod grid

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  console.log('Hello');
+  sleep(10000).then(() => { console.log('World!'); });
