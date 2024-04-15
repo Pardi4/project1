ba8d3f43865e3e01af8f5d128f47dac0bd8f9b8b
Zosia Samosia <Zosia@Samosia.pl>
2024-04-15 11:12:04 +0200

    add form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
 'use strict';
-
-// function sleep(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-//   }
-  
-//   console.log('Hello');
-//   sleep(10000).then(() => {window.location.href = "https://pl.cornhub.website"; });
